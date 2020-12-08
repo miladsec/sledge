bab3c7ecad84c2e22029f853cdae8478f94016fb
Milad Dehghan <milawddehghan@gmail.com>
2020-12-08 19:07:56 +0330

    remove element text

--- resources/views/element/text.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="form-group">
-    <label for="{{ $data['uniqueId'] }}">{{ $data['label'] }}</label>
-
-    <div class="controls">
-        <input type="text" value="{{ $data['value'] }}" name="{{ $data['name'] }}"
-               class="form-control input-label {{ $data['class'] }}" id="{{ $data['uniqueId'] }}"
-               placeholder="{{ $data['placeholder'] }}" autocomplete="off">
-    </div>
-</div>
