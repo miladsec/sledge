42c805b2a69fd9f4c44686e58cdad4484993f330
Milad Dehghan <milawddehghan@gmail.com>
2020-12-13 09:25:24 +0330

    change index template

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,67 +1,60 @@
-@extends('layouts.panel.app')
-@section('content')
-    <div class="app-content content">
-        <div class="content-overlay"></div>
-        <div class="content-wrapper">
-            {!!   $cc['navLink'] !!}
-            <div class="content-body">
-                <!-- Dashboard Ecommerce Starts -->
-                <section id="basic-datatable">
-                    <div class="row">
-                        <div class="col-12">
-                            <div class="card">
-                                <div class="card-header">
-                                    <h4 class="card-title">{{ $title ?? '' }}</h4>
-                                </div>
-                                <div class="card-content">
-                                    <div class="card-body card-dashboard">
-                                        @if(!empty($cc['metaData']))
-                                            <a href="{{ $cc['metaData']['url'] }}" class="btn btn-primary mr-1 mb-1">
-                                                <i class="{{ $cc['metaData']['icon'] }}"></i>
-                                                <span class="align-middle ml-25">{{ $cc['metaData']['text'] }}</span>
-                                            </a>
-                                        @endif
-                                        <button class="DeleteClass" id="footer-alert" href="#">TST</button>
+@extends('layouts.portal.app')
+@section('navLink')
+    {!!   $cc['navLink'] !!}
+@endsection
 
-                                        <div class="table-responsive">
-                                            <table id="tablename" class="table">
-                                                <thead>
-                                                <tr>
-                                                    @foreach($cc['table'] as $c)
-                                                        @if(isset($c['columnAction']))
-                                                            <th>{{ 'عملیات' }}</th>
-                                                        @else
-                                                            <th>{{ $c['text'] }}</th>
-                                                        @endif
-                                                    @endforeach
-                                                </tr>
-                                                </thead>
-                                                <tbody>
-                                                {{--@foreach($cc[0] as $key => $data)
-                                                    <tr>
-                                                        <td>{{ ++$key }}</td>
-                                                        @foreach($data as $k=>$d)
-                                                            <td>{!! $d !!} </td>
-                                                        @endforeach
-                                                    </tr>
-                                                @endforeach--}}
-                                                </tbody>
-                                                <tfoot>
-                                                <tr>
-                                                </tr>
-                                                </tfoot>
-                                            </table>
-                                        </div>
-                                    </div>
-                                </div>
+@section('content')
+    <section id="basic-datatable">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4 class="card-title">{{ $title ?? '' }}</h4>
+                    </div>
+                    <div class="card-content">
+                        <div class="card-body card-dashboard">
+                            @if(!empty($cc['metaData']))
+                                <a href="{{ $cc['metaData']['url'] }}" class="btn btn-primary mr-1 mb-1">
+                                    <i class="{{ $cc['metaData']['icon'] }}"></i>
+                                    <span class="align-middle ml-25">{{ $cc['metaData']['text'] }}</span>
+                                </a>
+                            @endif
+
+                            <div class="table-responsive">
+                                <table id="dataTable" class="table">
+                                    <thead>
+                                    <tr>
+                                        @foreach($cc['table'] as $c)
+                                            @if(isset($c['columnAction']))
+                                                <th>{{ 'عملیات' }}</th>
+                                            @else
+                                                <th>{{ $c['text'] }}</th>
+                                            @endif
+                                        @endforeach
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    {{--@foreach($cc[0] as $key => $data)
+                                        <tr>
+                                            <td>{{ ++$key }}</td>
+                                            @foreach($data as $k=>$d)
+                                                <td>{!! $d !!} </td>
+                                            @endforeach
+                                        </tr>
+                                    @endforeach--}}
+                                    </tbody>
+                                    <tfoot>
+                                    <tr>
+                                    </tr>
+                                    </tfoot>
+                                </table>
                             </div>
                         </div>
                     </div>
-                </section>
-                <!-- Dashboard Ecommerce ends -->
+                </div>
             </div>
         </div>
-    </div>
+    </section>
 
     <form id="deleteForm" style="display: none" action="" method="post">
         {{ csrf_field() }}
@@ -73,12 +66,12 @@
     <script>
         $(document).ready(function () {
 
-            $('#tablename').DataTable({
+            $('#dataTable').DataTable({
                 Processing: true,
                 serverSide: true,
                 "ajax": {
                     "type": "GET",
-                    "url": "{{url()->current()}}"+window.location.search,
+                    "url": '{{url()->current()}}'+window.location.search,
                     "dataSrc": function (json) {
                         return json.data;
                     },beforeSend: function() {
@@ -105,15 +98,15 @@
             });
 
             @if(Session::has('type'))
-                Swal.fire({
-                    position: 'top-center',
-                    type: '{{ Session::get('type') }}',
-                    title: '{{ Session::get('title') }}',
-                    showConfirmButton: false,
-                    timer: 2500,
-                    confirmButtonClass: 'btn btn-primary',
-                    buttonsStyling: false,
-                });
+            Swal.fire({
+                position: 'top-center',
+                type: '{{ Session::get('type') }}',
+                title: '{{ Session::get('title') }}',
+                showConfirmButton: false,
+                timer: 2500,
+                confirmButtonClass: 'btn btn-primary',
+                buttonsStyling: false,
+            });
             @endif
 
             $(document).on('click', 'a.confirm', function(e) {
@@ -143,23 +136,6 @@
                 });
             });
 
-            $('{!! $cc['confirm']['selector'] !!}').on('click', function () {
-                    Swal.fire({
-                        title: 'آیا مطمئنید؟',
-                        text: "این عمل قابل بازگشت نخواهد بود!",
-                        type: 'warning',
-                        showCancelButton: true,
-                        confirmButtonText: 'تایید',
-                        confirmButtonClass: 'btn btn-primary',
-                        cancelButtonClass: 'btn btn-danger ml-1',
-                        cancelButtonText: 'انصراف',
-                        buttonsStyling: false,
-                    }).then(function (result) {
-                        if (result.value) {
-                            {!!   $cc['confirm']['script'] !!}
-                        }
-                    });
-                });
 
             /*"ajax": {
                 "type": "GET",
