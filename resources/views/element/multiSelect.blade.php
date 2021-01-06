9193f41fcb4bff2242ae23e446b3190bcb779080
Milad Dehghan <milawddehghan@gmail.com>
2021-01-06 14:06:37 +0330

    multi title

--- resources/views/element/multiSelect.blade.php
+++ resources/views/element/multiSelect.blade.php
@@ -1,6 +1,11 @@
 @php
     $dKey0 = strval($data['dKey'][0]);
-    $dKey1 = strval($data['dKey'][1]);
+
+    if (!is_array($data['dKey'][1]))
+        $dKey1 = strval($data['dKey'][1]);
+    else
+        $dKey1 = $data['dKey'][1];
+
 @endphp
 <div class="form-group">
     <label for="{{ $data['uniqueId'] }}">{{ $data['label'] }}</label>
@@ -13,19 +18,64 @@
             id="{{ $data['uniqueId'] }} multiple-select2-icons"
             autocomplete="off"
             multiple
-            @foreach($data['validate'] as $key=>$validate)
-                {!!   ' '. $key .'="'. $validate .'" ' !!}
+        @foreach($data['validate'] as $key=>$validate)
+            {!!   ' '. $key .'="'. $validate .'" ' !!}
             @endforeach
         >
 
             @if($data['dKey'] == null)
                 @foreach($data['value'] as $value)
-                    <option value="{{ $value->$dKey0 }}">{{ $value->$dKey1 }}</option>
+                    <option value="{{ $value->$dKey0 }}">
+                        @php
+                            $secData = clone $value;
+                            if (is_array($dKey1)){
+                                $res = '';
+                                foreach ($dKey1 as $v){
+                                    $str = explode('.', $v);
+                                    if (count($str)>1){
+                                        for ($i = 0; $i < count($str); $i++) {
+                                            $value = $value->{$str[$i]};
+                                        }
+                                        $res .= $value . '-';
+
+                                        $value = $secData;
+                                    }else{
+                                        $res .= $value->$v . ' ';
+                                    }
+                                }
+                                echo $res;
+                            }else{
+                                echo $value->$dKey1;
+                            }
+                        @endphp
+                    </option>
                 @endforeach
             @else
                 @foreach($data['value'] as $value)
                     <option value="{{ $value->$dKey0 }}" {{ ( in_array($value->$dKey0, $data['old'])) ? ' selected ' : ''}}>
-                        {{ $value->$dKey1 }}</option>
+                        @php
+                            $secData = clone $value;
+                            if (is_array($dKey1)){
+                                $res = '';
+                                foreach ($dKey1 as $v){
+                                    $str = explode('.', $v);
+                                    if (count($str)>1){
+                                        for ($i = 0; $i < count($str); $i++) {
+                                            $value = $value->{$str[$i]};
+                                        }
+                                        $res .= $value . '-';
+
+                                        $value = $secData;
+                                    }else{
+                                        $res .= $value->$v . ' ';
+                                    }
+                                }
+                                echo $res;
+                            }else{
+                                echo $value->$dKey1;
+                            }
+                        @endphp
+                    </option>
                 @endforeach
             @endif
         </select>
