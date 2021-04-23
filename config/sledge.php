d468cdd01fdffe77280765cff5f2e496be1bf581
Milad Dehghan <milawddehghan@gmail.com>
2021-04-23 11:35:41 +0430

    add new config

--- config/sledge.php
+++ config/sledge.php
@@ -1,5 +1,5 @@
 <?php
-return[
+return [
     'validation' => [
         'required' => 'وارد کردن این فیلد  اجباری است'
     ],
@@ -11,5 +11,22 @@ return[
     ],
     'acl' => [
         'status' => true
-    ]
+    ],
+
+    'columnAction' => [
+        'route' => '<a class="dropdown-item *1" href="*2"><i class="*3 mr-1"></i>*4</a>',
+        'static' => '<div class="dropdown">
+                     <span class="bx bx-dots-vertical-rounded font-medium-3 dropdown-toggle nav-hide-arrow cursor-pointer" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" role="menu"></span>
+                     <div class="dropdown-menu">*1</div>
+                     </div>'
+    ],
+
+    'route' => [
+        'defaultRoute' => 'home',
+        'namePrefix' => [
+            'index' => 'لیست',
+            'create' => 'افزودن',
+            'edit' => 'ویرایش',
+        ]
+    ],
 ];
