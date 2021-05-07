e06efd7bf8efdf537324165996a8c5162bf65d30
Milad Dehghan <milawddehghan@gmail.com>
2021-05-07 13:23:51 +0430

    form

---
+++ src/Builder/Form.php
@@ -0,0 +1,137 @@
+<?php
+
+
+namespace MiladZamir\Sledge\Builder;
+
+
+use MiladZamir\Sledge\Helper\Helper;
+
+class Form
+{
+    public $headerData;
+    public $bodyData;
+    public $footerData;
+    public $config;
+    public $name;
+    public $id;
+    public $cssClass;
+    public $novalidate;
+    public $enctype;
+    public $acceptCharset;
+    public $autocomplete;
+    public $type;
+    public $label;
+    public $validate;
+    public $value;
+    public $placeholder;
+    public $uniqueId;
+
+
+    public function __construct($config)
+    {
+        $this->config = $config;
+        $this->uniqueId = Helper::createUniqueString(5);
+    }
+
+    public function open(): Form
+    {
+        $this->headerData = view('sledge::structure.openForm')->with('data', $this);
+        return $this;
+    }
+
+    public function close()
+    {
+        $this->footerData = view('sledge::structure.closeForm');
+        return $this;
+    }
+    public function input($type): Form
+    {
+        $this->type = $type;
+
+        switch ($this->type){
+            case 'text':
+            case 'number':
+                $this->bodyData = view('sledge::element.input')->with('data', $this);
+                break;
+            case 'submit':
+                $this->footerData = view('sledge::element.submit')->with('data', $this);
+                break;
+            default:
+                dd('!');
+        }
+
+        return $this;
+    }
+
+    public function name($name): Form
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function placeholder($placeholder): Form
+    {
+        $this->placeholder = $placeholder;
+        return $this;
+    }
+
+    public function validate(array $validate): Form
+    {
+        $this->validate = '';
+        foreach ($validate as $value){
+            foreach ($value as $key => $v){
+                $this->validate .= ' '. $key .'="'. $v .'" ';
+            }
+        }
+        return $this;
+    }
+
+    public function value($value): Form
+    {
+        $this->value = $value;
+        return $this;
+    }
+
+    public function label($label): Form
+    {
+        $this->label = $label;
+        return $this;
+    }
+
+    public function id($id): Form
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function cssClass($cssClass): Form
+    {
+        $this->cssClass = $cssClass;
+        return $this;
+    }
+
+    public function novalidate($novalidate): Form
+    {
+        $this->novalidate = $novalidate;
+        return $this;
+    }
+
+    public function enctype($enctype): Form
+    {
+        $this->enctype = $enctype;
+        return $this;
+    }
+
+    public function autocomplete($autocomplete = 'off'): Form
+    {
+        $this->autocomplete = $autocomplete;
+        return $this;
+    }
+
+    public function acceptCharset($acceptCharset = 'utf-8'): Form
+    {
+        $this->acceptCharset = $acceptCharset;
+        return $this;
+    }
+
+}
