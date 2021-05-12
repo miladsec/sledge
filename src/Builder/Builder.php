fec007517f6092cdd954920dbe148195167a8e77
Milad Dehghan <milawddehghan@gmail.com>
2021-05-12 11:01:24 +0430

    remove old methods

--- src/Builder/Builder.php
+++ src/Builder/Builder.php
@@ -150,7 +150,6 @@ class Builder
                 }
             }
         }
-
         return ['table' => $this->table, 'button' => $this->config->button, 'navbar' => $this->config->navbar, 'form' => $this->formData];
     }
 
@@ -158,38 +157,6 @@ class Builder
     {
         $this->form[] = new Form($this->config);
         return end($this->form);
-    }
-
-    public function openForm($name = null, $enctype = null, $novalidate = 'novalidate', $autocomplete = 'off', $accept_charset = 'utf-8', $class = null, $id = null)
-    {
-        $data = [
-            'action' => $this->formAction,
-            'method' => [$this->formMethod, $this->formMethodField],
-            'name' => $name,
-            'enctype' => $enctype,
-            'novalidate' => $novalidate,
-            'autocomplete' => $autocomplete,
-            'accept-charset' => $accept_charset,
-            'class' => $class,
-            'id' => $id,
-        ];
-        array_push($this->data['header'], view('sledge::structure.openForm')->with('data', $data));
-    }
-
-    public function input($type, $name, $label, $validate = [], $value = null, $placeholder = null, $class = null, $id = null)
-    {
-        $data = [
-            'uniqueId' => Helper::createUniqueString(5),
-            'type' => $type,
-            'name' => $name,
-            'value' => $value,
-            'validate' => $validate,
-            'label' => $label,
-            'placeholder' => $placeholder,
-            'class' => $class,
-            'id' => $id,
-        ];
-        array_push($this->data['body'], view('sledge::element.input')->with('data', $data));
     }
 
     public function file($name, $label, $validate = [], $value = null, $placeholder = null, $size = [], $class = null, $id = null)
@@ -206,41 +173,6 @@ class Builder
             'id' => $id,
         ];
         array_push($this->data['body'], view('sledge::element.file')->with('data', $data));
-    }
-
-    public function select($name, $label, $dKey, $validate = [], $value, $old = null, $placeholder = null, $class = null, $id = null)
-    {
-        $data = [
-            'uniqueId' => Helper::createUniqueString(5),
-            'name' => $name,
-            'label' => $label,
-            'dKey' => $dKey,
-            'validate' => $validate,
-            'value' => $value,
-            'old' => $old,
-            'placeholder' => $placeholder,
-            'class' => $class,
-            'id' => $id,
-        ];
-
-        array_push($this->data['body'], view('sledge::element.select')->with('data', $data));
-    }
-
-    public function multiSelect($name, $label, $dKey, $validate = [], $value, $old = null, $placeholder = null, $class = null, $id = null)
-    {
-        $data = [
-            'uniqueId' => Helper::createUniqueString(5),
-            'name' => $name,
-            'label' => $label,
-            'dKey' => $dKey,
-            'validate' => $validate,
-            'value' => $value,
-            'old' => $old,
-            'placeholder' => $placeholder,
-            'class' => $class,
-            'id' => $id,
-        ];
-        array_push($this->data['body'], view('sledge::element.multiSelect')->with('data', $data));
     }
 
     public function checkbox($name, $label, $dKey, $validate = [], $value, $old = null, $class = null, $id = null)
@@ -330,23 +262,6 @@ class Builder
     public function customView($src, $data = null, $col = 'col-6')
     {
         array_push($this->data['body'], view('sledge::' . $src)->with(compact('data', 'col')));
-    }
-
-    public function submit($value, $name = null, $class = null, $id = null)
-    {
-        $data = [
-            'value' => $value,
-            'name' => $name,
-            'class' => $class,
-            'id' => $id
-        ];
-
-        array_push($this->data['footer'], view('sledge::element.submit')->with('data', $data));
-    }
-
-    public function closeForm()
-    {
-        array_push($this->data['footer'], view('sledge::structure.closeForm'));
     }
 
     public function setScript($bladeFile, $data = null)
