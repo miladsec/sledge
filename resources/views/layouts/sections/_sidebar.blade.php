aa5e781596cb6149081813a0ae650dfa123410b6
Milad Dehghan <milawddehghan@gmail.com>
2021-06-27 20:07:26 +0430

    view example

--- resources/views/layouts/sections/_sidebar.blade.php
+++ resources/views/layouts/sections/_sidebar.blade.php
@@ -1,38 +1,7 @@
-<div class="main-menu menu-fixed menu-dark menu-accordion menu-shadow" data-scroll-to-active="true">
-    <div class="navbar-header">
-        <ul class="nav navbar-nav flex-row">
-            <li class="nav-item mr-auto"><a class="navbar-brand"
-                                            href="/src/html/vertical-menu-template-dark/index.html">
-                    <div class="brand-logo"><img class="logo" src="/src/assets/images/logo/logo.png"></div>
-                    <h2 class="brand-text mb-0">Frest</h2></a></li>
-            <li class="nav-item nav-toggle"><a class="nav-link modern-nav-toggle pr-0" data-toggle="collapse"><i
-                        class="bx bx-x d-block d-xl-none font-medium-4 primary"></i><i
-                        class="toggle-icon bx bx-disc font-medium-4 d-none d-xl-block primary" data-ticon="bx-disc"></i></a>
-            </li>
-        </ul>
-    </div>
-    <div class="shadow-bottom"></div>
+<div>
     <div class="main-menu-content">
-        <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation"
-            data-icon-style="lines">
-            <li class=" nav-item"><a href="/src/html/vertical-menu-template-dark/index.html"><i class="menu-livicon"
-                                                                                                data-icon="desktop"></i><span
-                        class="menu-title" data-i18n="Dashboard">داشبورد</span><span
-                        class="badge bg-rgba-danger text-danger badge-pill badge-round float-right mr-2">2</span></a>
-                <ul class="menu-content">
-                    <li><a href="dashboard-ecommerce.html"><i class="bx bx-left-arrow-alt"></i><span class="menu-item"
-                                                                                                     data-i18n="eCommerce">تجارت الکترونیک</span></a>
-                    </li>
-                    <li><a href="dashboard-analytics.html"><i class="bx bx-left-arrow-alt"></i><span class="menu-item"
-                                                                                                     data-i18n="Analytics">آمار تحلیلی</span></a>
-                    </li>
-                </ul>
-            </li>
-            <li class=" navigation-header"><span>برنامه ها</span>
-            </li>
-            <li class="disabled nav-item"><a href="#"><i class="menu-livicon" data-icon="morph-preview"></i><span
-                        class="menu-title" data-i18n="Disabled Menu">گزینه غیرفعال</span></a>
-            </li>
-        </ul>
+       <ul>
+           <li></li>
+       </ul>
     </div>
 </div>
--- resources/views/layouts/sections/breadcrumb.blade.php
+++ resources/views/layouts/sections/breadcrumb.blade.php
@@ -1,13 +1,7 @@
-<div class="content-header-left col-12 mb-2 mt-1">
-    <div class="row breadcrumbs-top">
-        <div class="col-12">
-            <div class="breadcrumb-wrapper">
-                <ol class="breadcrumb p-0 mb-0">
-                    @foreach($sledge['breadcrumb'] as $breadcrumb=>$route)
-                        <li class="breadcrumb-item"><a href="{{ $route }}">{!! $breadcrumb !!}</a></li>
-                    @endforeach
-                </ol>
-            </div>
-        </div>
-    </div>
+<div>
+    <ol>
+        @foreach($sledge['breadcrumb'] as $breadcrumb=>$route)
+            <li><a href="{{ $route }}">{!! $breadcrumb !!}</a></li>
+        @endforeach
+    </ol>
 </div>
