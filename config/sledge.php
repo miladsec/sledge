ffe9b81d0b886da04fc4cd242f61b3d6878d9ff0
Milad Dehghan <milawddehghan@gmail.com>
2021-06-27 20:09:03 +0430

    sledge config

--- config/sledge.php
+++ config/sledge.php
@@ -8,17 +8,21 @@ return [
         'addLinkText' => 'افزودن',
         'addLinkIcon' => 'bx bx-plus',
         'dropdown' => false,
-        'actionColumnName' => 'عملیاتx'
+        'actionColumnName' => 'عملیات'
     ],
     'create' => [
-      'defaultCol' => 'col-md-6'
+        'defaultCol' => 'col-md-6'
+    ],
+    'alert' =>[
+        'success' => 'ok :)',
+        'danger' => 'Nok :('
     ],
     'acl' => [
         'status' => true
     ],
 
     'columnAction' => [
-        'route' => '<a class="dropdown-item *1" href="*2"><i class="*3 mr-1"></i>*4</a>',
+        'route' => '<a class="dropdown-item *1" href="*2">*3 *4</a>',
         'static' => '<div class="dropdown">
                      <span class="bx bx-dots-vertical-rounded font-medium-3 dropdown-toggle nav-hide-arrow cursor-pointer" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" role="menu"></span>
                      <div class="dropdown-menu">*1</div>
